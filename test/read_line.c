6363f9ef830e842e57b19a869781e6637f8cdb68
Lisa Martin <lismarti@bess-f3r1s14.clusters.42paris.fr>
2025-02-04 09:23:08 +0100

    code test read line

---
+++ test/read_line.c
@@ -0,0 +1,51 @@
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <stdlib.h>
+#include <signal.h>
+
+char    *get_line()
+{
+    char *line;
+
+    line = NULL;
+    if (line)
+    {
+        free(line);
+        line = NULL;
+    }
+    line = readline("Minishell>");
+    if (line)
+        add_history(line);
+    return (line);
+}
+
+void    sig_handler(int signum)
+{
+    if (signum == SIGINT)
+    {
+        printf("\n");
+        rl_on_new_line();
+        rl_replace_line("", 1);
+        rl_redisplay();
+    }
+}
+
+int main(void)
+{
+    char    *line;
+
+    while(1)
+    {signal(SIGINT, sig_handler);
+    line = get_line();}
+}
+
+
+
+
+
+
+
+
+
