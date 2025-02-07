040e697a472ec84bc1a8209b2540284cf827eed0
Lisa Martin <lismarti@bess-f4r2s8.clusters.42paris.fr>
2025-02-07 10:55:34 +0100

    argc, argv, envp

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -6,16 +6,17 @@ void exit_minishell()
     exit(0);
 }
 
-int main(void)
+int main(int argc, char** argv, char** envp)
 {
     char* str;
-    
+    if (argc != 1)
+        return(1);
     while(1)
     {
         str = readline("minishell>");
         if (str == NULL)
             exit_minishell();   
         add_history(str);
-        creat_list(str);
+        creat_list(str, envp, argv, argc);
     }
 }
