8d54b9be9dba8e4d5acb02d9d74912aba43f90d6
Sebastian Mendez-Pineda <smendez-@paul-f4Ar5s3.clusters.42paris.fr>
2025-02-10 10:47:36 +0100

    resulving merge conflicts

--- minishell/exe1.c
+++ minishell/exe1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exe1.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lismarti <lismarti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: smendez- <smendez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 11:57:04 by smendez-          #+#    #+#             */
-/*   Updated: 2025/02/07 19:21:04 by lismarti         ###   ########.fr       */
+/*   Updated: 2025/02/07 19:19:59 by smendez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,7 +100,7 @@ void type1(t_list *pip)
 	}
         fd_out = open(pip->if_file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	ft_printf_fd(2, "fd: %d, file: |%s|", fd_out, pip->if_file2 );
-	(dup2(fd_out, STDOUT_FILENO), close(fd_out), ft_close_all(pip->data->fd));
+	(dup2(fd_out, STDOUT_FILENO), close(fd_out));
 }
 
 void type2(t_list *pip)
@@ -108,7 +108,6 @@ void type2(t_list *pip)
 	ft_printf_fd(2, "cmd1: fd: %d\n\n", pip->index - 1);
         if (dup2(pip->data->fd[pip->index - 1][1], STDOUT_FILENO) == -1)
 		(perror("dup2 1"), exit(EXIT_FAILURE));
-	ft_close_all(pip->data->fd);
 }
 
 void type3(t_list *pip)
@@ -121,24 +120,8 @@ void type3(t_list *pip)
 		(free_pip(pip), exit(EXIT_FAILURE));
 	}
         fd_out = open(pip->if_file2, O_APPEND | O_WRONLY | O_CREAT, 0644);
-	(dup2(fd_out, STDOUT_FILENO), close(fd_out), ft_close_all(pip->data->fd));
+	(dup2(fd_out, STDOUT_FILENO), close(fd_out));
 }
-
-// void type_inout(t_list *pip, int *i, int t1, int t2)
-// {
-// 	if (t1 == 6) 
-//                 type6(pip, &i);
-//         else if (t1 == 5)
-//                 type5(pip, i);
-// 	if (t2 == 0)
-//                 i--;
-//         else if (t2 == 1)
-//                 type1(pip, &i);
-// 	else if (t2 == 2)
-//                 type2(pip);
-// 	else if (t2 == 3)
-//                 type3(pip, &i);
-// }
 
 
 void	exe_isolate(t_list *pip, int t1, int t2)
@@ -146,7 +129,6 @@ void	exe_isolate(t_list *pip, int t1, int t2)
 	char	**temp2;
 	char	*no_a;
 	char	*get_p;
-        int		open_fd;
 
         if (t1 == 6) 
                 type6(pip);
@@ -158,6 +140,7 @@ void	exe_isolate(t_list *pip, int t1, int t2)
                 type2(pip);
 	else if (t2 == 3)
                 type3(pip);
+	ft_close_all(pip->data->fd);
 	temp2 = ft_split_exe(pip->cmd, ' ');
 	no_a = no_args_cmd(pip->cmd);
 	get_p = get_path_command(pip->data->path, no_a);
