8fe25515c638d65e6a339e48c638c116aabd999d
Lisa Martin <lismarti@bess-f3r1s12.clusters.42paris.fr>
2025-03-07 12:12:13 +0100

    moiie lib

--- minishell/lib/split_ope_bis.c
+++ minishell/lib/split_ope_bis.c
@@ -6,7 +6,7 @@
 /*   By: lismarti <lismarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:35:37 by smendez-          #+#    #+#             */
-/*   Updated: 2025/03/06 14:58:24 by lismarti         ###   ########.fr       */
+/*   Updated: 2025/03/07 11:51:17 by lismarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,14 @@ static char	*t2f(char const *s, int start_s, char c)
 	return (t2);
 }
 
+int	split2(char const *s, int i)
+{
+	if (s[i] == '"' || s[i] == '\'')
+		i = pass_quote_plus(s[i], s, i + 1);
+	i++;
+	return (i);
+}
+
 char	**ft_split_ope_bis(char const *s, char c, int i, int j)
 {
 	char	**t1;
@@ -76,11 +84,7 @@ char	**ft_split_ope_bis(char const *s, char c, int i, int j)
 	while (s[i])
 	{
 		while (s[i] == c && s[i])
-		{
-			if (s[i] == '"' || s[i] == '\'')
-				i = pass_quote_plus(s[i], s, i + 1);
-			i++;
-		}
+			i = split2(s, i);
 		if (!s[i])
 			break ;
 		t1[j] = t2f(s, i, c);
--- minishell/lib/split_txt.c
+++ minishell/lib/split_txt.c
@@ -6,24 +6,19 @@
 /*   By: lismarti <lismarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 17:14:14 by lismarti          #+#    #+#             */
-/*   Updated: 2025/03/06 14:49:39 by lismarti         ###   ########.fr       */
+/*   Updated: 2025/03/07 12:11:26 by lismarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static int	splitlen6(char const *s1)
+static int	splitlen6(char const *s1, int i, int k)
 {
-	int	i;
-	int	k;
-
-	i = 0;
-	k = 0;
 	while (s1[i] != '\0')
 	{
-		if (s1[i] == '|' || s1[i] == '<' || s1[i] == '>' || s1[i] == '&')
+		if (s1[i] == '|' || s1[i] == '<' || s1[i] == '>')
 			k++;
-		while ((s1[i] == '|' || s1[i] == '<' || s1[i] == '>' || s1[i] == '&')
+		while ((s1[i] == '|' || s1[i] == '<' || s1[i] == '>')
 			&& s1[i] != '\0')
 		{
 			if (s1[i] == '"' || s1[i] == '\'')
@@ -32,7 +27,7 @@ static int	splitlen6(char const *s1)
 		}
 		if (s1[i])
 			k++;
-		while ((s1[i] != '|' && s1[i] != '<' && s1[i] != '>' && s1[i] != '&')
+		while ((s1[i] != '|' && s1[i] != '<' && s1[i] != '>')
 			&& s1[i] != '\0')
 		{
 			if (s1[i] == '"' || s1[i] == '\'')
@@ -43,6 +38,20 @@ static int	splitlen6(char const *s1)
 	return (k);
 }
 
+int t3f(char const* s, int start_s, int i)
+{
+	if (i == start_s)
+	{
+		while (s[i] != '|' && s[i] != '<' && s[i] != '>' && s[i])
+		{
+			if (s[i] == '"' || s[i] == '\'')
+				i = pass_quote_plus(s[i], s, i + 1);
+			i++;
+		}
+	}
+	return (i);
+}
+
 static char	*t2f(char const *s, int start_s)
 {
 	int		i;
@@ -50,22 +59,13 @@ static char	*t2f(char const *s, int start_s)
 	char	*t2;
 
 	i = start_s;
-	while ((s[i] == '|' || s[i] == '<' || s[i] == '>' || s[i] == '&') && s[i])
+	while ((s[i] == '|' || s[i] == '<' || s[i] == '>') && s[i])
 	{
 		if (s[i] == '"' || s[i] == '\'')
 			i = pass_quote_plus(s[i], s, i + 1);
 		i++;
 	}
-	if (i == start_s)
-	{
-		while ((s[i] != '|' && s[i] != '<' && s[i] != '>' && s[i] != '&')
-			&& s[i])
-		{
-			if (s[i] == '"' || s[i] == '\'')
-				i = pass_quote_plus(s[i], s, i + 1);
-			i++;
-		}
-	}
+	i = t3f(s, start_s, i);
 	len_s = i - start_s;
 	i = 0;
 	t2 = malloc((len_s + 1) * sizeof(char));
@@ -79,42 +79,39 @@ static char	*t2f(char const *s, int start_s)
 	t2[i] = '\0';
 	return (t2);
 }
+int split_bis(char const* s, int i)
+{
+			while (s[i] == '|' || s[i] == '<' || s[i] == '>' && s[i])
+			{
+				if (s[i] == '"' || s[i] == '\'')
+					i = pass_quote_plus(s[i], s, i + 1);
+				i++;
+			}
+			return(i);
+}
 
-char	**ft_split_txt(char const *s)
+char	**ft_split_txt(char const *s, int i, int j)
 {
 	char	**t1;
-	int		i;
-	int		j;
 	int		check;
 
-	t1 = malloc((splitlen6(s) + 1) * sizeof(char *));
+	t1 = malloc((splitlen6(s, 0, 0) + 1) * sizeof(char *));
 	if (t1 == NULL)
 		return (NULL);
-	i = 0;
-	j = 0;
 	while (s[i])
 	{
 		t1[j] = t2f(s, i);
 		if (t1[j++] == NULL)
 			return (NULL);
 		check = i;
-		while ((s[i] != '|' && s[i] != '<' && s[i] != '>' && s[i] != '&')
-			&& s[i])
+		while (s[i] != '|' && s[i] != '<' && s[i] != '>' && s[i])
 		{
 			if (s[i] == '"' || s[i] == '\'')
 				i = pass_quote_plus(s[i], s, i + 1);
 			i++;
 		}
 		if (i == check)
-		{
-			while ((s[i] == '|' || s[i] == '<' || s[i] == '>' || s[i] == '&')
-				&& s[i])
-			{
-				if (s[i] == '"' || s[i] == '\'')
-					i = pass_quote_plus(s[i], s, i + 1);
-				i++;
-			}
-		}
+			i = split_bis(s, i);
 	}
 	return (t1[j] = NULL, t1);
 }
